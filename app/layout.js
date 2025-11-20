4665e72a93221eb170f2e66c0050082d4d66117a
DineshChokkai <160124232+DineshChokkai@users.noreply.github.com>
2025-11-20 14:10:42 +0530

    css updated

--- app/layout.js
+++ app/layout.js
@@ -41,12 +41,12 @@ export const metadata = {
   // EXTRA: Microsoft Teams + WhatsApp required meta tags
   other: {
     "og:image": "https://sense-original-nextjs.vercel.app/opengraph-image.png",
-    "og:image:width": "600",
+    "og:image:width": "1200",
     "og:image:height": "630",
     "msapplication-TileImage":
       "https://sense-original-nextjs.vercel.app/opengraph-image.png",
     "msapplication-TileColor": "#ffffff",
-    "theme-color": "#ffffff",
+    
   },
 };
 
